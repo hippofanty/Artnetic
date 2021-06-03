2f79d67ab50d7aa2a8760a19b54a8c98f0c0aacb
Michael <micklekoltsov@gmail.com>
2021-06-03 11:35:25 +0300

    Profile in progress

---
+++ front/src/components/Profile/Information/Information.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box } from '@material-ui/core';
+
+export const Information = () => {
+  return (
+    <Box>
+      <h1> Информация профиля</h1>
+    </Box>
+  )
+}
