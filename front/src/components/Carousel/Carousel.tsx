70d03355c3ff6bfcbe34a6bd1cef8d6524152e14
Michael <micklekoltsov@gmail.com>
2021-06-10 18:04:03 +0300

    Gallery styling

--- front/src/components/Carousel/Carousel.tsx
+++ front/src/components/Carousel/Carousel.tsx
@@ -1,19 +1,34 @@
+import { makeStyles, Theme, Typography } from '@material-ui/core';
 import React, { ReactNode, useEffect, useState } from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 import { Link } from 'react-router-dom';
 import { Work } from '../../redux/init';
 
+const useStyles = makeStyles((theme: Theme) => ({
+  searchTitleDivider: {
+    borderTop: '5px solid rgb(238, 238, 238)',
+		border: 0,
+		height: 0,
+    width: 500,
+    marginBottom: 20,
+  },
+  searchtitle: {
+		width: '100%',
+		textAlign: 'center',
+		marginBottom: '50px',
+		textTransform: 'uppercase',
+    fontFamily: `'Montserrat', sans-serif`,
+    fontSize: '36px',
+	},
+}));
+
 const handleDragStart = (e: any) => e.preventDefault();
 
-const items: ReactNode[] = [
-  // <img src="path-to-img" onDragStart={handleDragStart} alt="carousel_item1" />,
-  // <img src="path-to-img" onDragStart={handleDragStart} alt="carousel_item2" />,
-  // <img src="path-to-img" onDragStart={handleDragStart} alt="carousel_item3" />,
-];
+const items: ReactNode[] = [];
 
 export const Gallery = () => {
-  // const [works, setWorks] = useState<Work[]>();
+  const classes = useStyles();
   const [workItems, setWorkItems] = useState<ReactNode[]>();
   // Get posts for carousel
   const itemsLoader = async () => {
@@ -43,23 +58,29 @@ export const Gallery = () => {
   }, []);
 
   return (
-    <AliceCarousel
-      mouseTracking
-      disableDotsControls={true}
-      autoPlay={true}
-      animationDuration={1500}
-      autoPlayInterval={2000}
-      infinite={true}
-      items={workItems}
-      responsive={{
-        0: {
-          items: 1,
-        },
-        1440: {
-          items: 6,
-        },
-      }}
-      autoWidth={true}
-    />
+    <>
+      <Typography variant="h4" className={classes.searchtitle}>
+        featured artwork
+      </Typography>
+			<hr className={classes.searchTitleDivider} />
+      <AliceCarousel
+        mouseTracking
+        disableDotsControls={true}
+        autoPlay={true}
+        animationDuration={1500}
+        autoPlayInterval={2000}
+        infinite={true}
+        items={workItems}
+        responsive={{
+          0: {
+            items: 1,
+          },
+          1440: {
+            items: 6,
+          },
+        }}
+        autoWidth={true}
+      />
+    </>
   );
 };
