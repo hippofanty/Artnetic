d9a36e429de1098ed2616eff4e3db4d559fb0bbb
Michael <micklekoltsov@gmail.com>
2021-06-11 14:47:58 +0300

    Main page about

--- front/src/components/AboutUsMain/AboutUsMain.tsx
+++ front/src/components/AboutUsMain/AboutUsMain.tsx
@@ -17,22 +17,43 @@ const useStyles = makeStyles((theme: Theme) => ({
     fontFamily: `'Montserrat', sans-serif`,
     fontSize: '36px',
 	},
+  info: {
+    fontFamily: `'Montserrat', sans-serif`,
+    textAlign: "center",
+    marginBottom: 70,
+  },
+  footer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontFamily: `'Montserrat', sans-serif`,
+    textAlign: "center",
+    height: 150,
+    width: '100%',
+    marginTop: 40,
+    borderTop: '1px solid black',
+  }
 }));
 
 export const AboutUsMain = () => {
   const classes = useStyles();
 
 	return (
-		<Container fixed>
-			<div>
-        <Typography variant="h4" className={classes.searchtitle}>
-          what we are
-        </Typography>
-        <hr className={classes.searchTitleDivider} />
-        <Typography>
-          
-        </Typography>
-			</div>
-		</Container>
+    <>
+      <Container fixed>
+        <div>
+          <Typography variant="h4" className={classes.searchtitle}>
+            what we are
+          </Typography>
+          <hr className={classes.searchTitleDivider} />
+          <Typography variant="h5" component="h2" className={classes.info}>
+          ARTNETIC has built a reputation as the leading source for high-quality, cleared artwork for set decoration in a large range of styles and genres. Known for our superb client service, quick turnaround, and deep connections in the art world, we will do whatever it takes to realize your vision on your timeline.
+          </Typography>
+        </div>
+      </Container>
+      <div className={classes.footer}>
+        <div><p>Art Division of Elbrus-Bootcamp, 2021.</p></div>
+      </div>
+    </>
 	);
 };
