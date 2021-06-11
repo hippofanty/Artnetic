d76457ef2352cda519e33c5f35c2aaf99d42b395
Olga <olga.dibts@ya.ru>
2021-06-11 12:30:56 +0300

    спрятан input file

--- front/src/components/Profile/Information/UploadAvatar.tsx
+++ front/src/components/Profile/Information/UploadAvatar.tsx
@@ -26,7 +26,7 @@ const useStyles = makeStyles((theme: Theme) =>
       alignItems: "center",
     },
     fileInput: {
-      // display: "none",
+      display: "none",
       color: "green",
     },
     customFileUpload: {
